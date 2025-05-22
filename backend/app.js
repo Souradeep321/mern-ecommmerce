2e40a0479373f84c9e81a6158f93ceacc8873d92
Souradeep321 <souradeephazra93@gmail.com>
2025-05-22 19:55:33 +0530

    Analytics Routes done

--- backend/app.js
+++ backend/app.js
@@ -22,6 +22,7 @@ import productRoutes from './routes/product.routes.js'
 import cartRoutes from './routes/cart.routes.js'
 import couponRoutes from './routes/coupon.routes.js'
 import paymentRoutes from './routes/payment.routes.js'
+import analyticsRoutes from './routes/analytics.routes.js'
 
 // routes
 app.use("/api/v1/auth", authRoutes)
@@ -29,6 +30,7 @@ app.use("/api/v1/products", productRoutes)
 app.use("/api/v1/cart", cartRoutes)
 app.use("/api/v1/coupons", couponRoutes)
 app.use("/api/v1/payments", paymentRoutes)
+app.use("/api/v1/analytics", analyticsRoutes)
 
 connectDb()
     .then(() => {
---
+++ backend/routes/analytics.routes.js
@@ -0,0 +1,26 @@
+import { Router } from "express";
+import { protectRoute, adminRoute } from "../middlewares/auth.middlewares.js";
+import { getAnalyticsData } from "../controllers/analytics.controllers.js";
+
+const router = Router();
+
+router.route("/").get(protectRoute, adminRoute, async (req, res) => {
+    try {
+        const analyticsData = await getAnalyticsData();
+
+        const endDate = new Date();
+        const startDate = new Date(endDate.getTime() - 7 * 24 * 60 * 60 * 1000); // 7 days ago
+
+        const dailySalesData = await getDailySalesData(startDate, endDate);
+
+        res.status(200).json({
+            analyticsData,
+            dailySalesData
+        });
+    } catch (error) {
+        console.log("Error in analytics route", error.message);
+        res.status(500).json({ message: "Something went wrong", error: error.message });
+    }
+});
+
+export default router
